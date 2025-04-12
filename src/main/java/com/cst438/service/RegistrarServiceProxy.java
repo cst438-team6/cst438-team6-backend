9063238c45e3268f28efeb7135fb1cdea08a9d8b
Thomas Wood <thwood@csumb.edu>
2025-04-11 22:53:34 -0700

    implemented RegistrarServiceProxy.receiveFromRegistrar(String message)

--- src/main/java/com/cst438/service/RegistrarServiceProxy.java
+++ src/main/java/com/cst438/service/RegistrarServiceProxy.java
@@ -1,11 +1,14 @@
 package com.cst438.service;
 
+import com.cst438.domain.*;
+import com.cst438.dto.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,10 +23,105 @@ public class RegistrarServiceProxy {
 
     @Autowired
     RabbitTemplate rabbitTemplate;
+    @Autowired
+    CourseRepository courseRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    TermRepository termRepository;
+    @Autowired
+    SectionRepository sectionRepository;
+    @Autowired
+    EnrollmentRepository enrollmentRepository;
 
     @RabbitListener(queues = "gradebook_service")
     public void receiveFromRegistrar(String message)  {
-        //TODO implement this message
+        try{
+            String[] parts =  message.split(" ", 2);
+            String action = parts[0];
+            // (prof's code)SimpleJpaRepository<T, Course> courseRepository;
+            if (action.equals("addCourse")) {
+                CourseDTO dto = fromJsonString(parts[1], CourseDTO.class);
+                Course c = new Course();
+                c.setCourseId(dto.courseId());
+                c.setTitle(dto.title());
+                c.setCredits(dto.credits());
+                courseRepository.save(c);
+            } else if (action.equals("deleteCourse")) {
+                courseRepository.deleteById(parts[1]);
+            } else if (action.equals("updateCourse")) {
+                CourseDTO dto = fromJsonString(parts[1], CourseDTO.class);
+                Course c = courseRepository.findById(dto.courseId()).orElse(null);
+                c.setTitle(dto.title());
+                c.setCredits(dto.credits());
+                courseRepository.save(c);
+            } else if (action.equals("addUser")) {
+                UserDTO dto = fromJsonString(parts[1], UserDTO.class);
+                User user = new User();
+                user.setId(dto.id());
+                user.setName(dto.name());
+                user.setEmail(dto.email());
+                user.setType(dto.type());
+                userRepository.save(user);
+            } else if (action.equals("deleteUser")) {
+                userRepository.deleteById(Integer.parseInt(parts[1]));
+            } else if (action.equals("updateUser")) {
+                UserDTO dto = fromJsonString(parts[1], UserDTO.class);
+                User user = userRepository.findById(dto.id()).orElse(null);
+                user.setName(dto.name());
+                user.setEmail(dto.email());
+                user.setType(dto.type());
+                userRepository.save(user);
+            } else if (action.equals("addSection")) {
+                SectionDTO dto = fromJsonString(parts[1], SectionDTO.class);
+                Term term = termRepository.findByYearAndSemester(dto.year(), dto.semester());
+                Course c = courseRepository.findById(dto.courseId()).orElse(null);
+                Section section = new Section();
+                section.setCourse(c);
+                section.setSectionNo(dto.secNo());
+                section.setBuilding(dto.building());
+                section.setRoom(dto.room());
+                section.setTerm(term);
+                section.setCourse(courseRepository.findById(dto.courseId()).orElse(null));
+                section.setInstructor_email(dto.instructorEmail());
+                section.setTimes(dto.times());
+                section.setSecId(dto.secId());
+                sectionRepository.save(section);
+            } else if (action.equals("deleteSection")) {
+                sectionRepository.deleteById(Integer.parseInt(parts[1]));
+            } else if (action.equals("updateSection")) {
+                SectionDTO dto = fromJsonString(parts[1], SectionDTO.class);
+                Section section = sectionRepository.findById(dto.secNo()).orElse(null);
+                Term term = termRepository.findByYearAndSemester(dto.year(), dto.semester());
+                Course c = courseRepository.findById(dto.courseId()).orElse(null);
+                section.setCourse(c);
+                section.setSectionNo(dto.secNo());
+                section.setBuilding(dto.building());
+                section.setRoom(dto.room());
+                section.setTerm(term);
+                section.setCourse(courseRepository.findById(dto.courseId()).orElse(null));
+                section.setInstructor_email(dto.instructorEmail());
+                section.setTimes(dto.times());
+                section.setSecId(dto.secId());
+                sectionRepository.save(section);
+            } else if (action.equals("addEnrollment")) {
+                EnrollmentDTO dto = fromJsonString(parts[1], EnrollmentDTO.class);
+                Enrollment enrollment = new Enrollment();
+                enrollment.setEnrollmentId(dto.enrollmentId());
+                User student = userRepository.findById(dto.studentId()).orElse(null);
+                enrollment.setStudent(student);
+                //enrollment.setGrade(dto.grade()); Will be null
+                Section section = sectionRepository.findById(dto.sectionId()).orElse(null);
+                enrollment.setSection(section);
+                enrollmentRepository.save(enrollment);
+            } else if (action.equals("deleteEnrollment")) {
+                sectionRepository.deleteById(Integer.parseInt(parts[1]));
+            } else{
+                System.out.println("Invalid action");
+            }
+        } catch (Exception e) {
+            System.out.println("Exception in recieveFromRegistrar " + e.getMessage());
+        }
     }
 
 
