02de71ed5a6bb292aa10684d9812f3cd74fd89ac
Zachary Davis <zachary.davis0502@gmail.com>
2025-04-13 15:36:02 -0700

    implemented receiveFromGradebook

--- src/main/java/com/cst438/service/GradebookServiceProxy.java
+++ src/main/java/com/cst438/service/GradebookServiceProxy.java
@@ -1,5 +1,8 @@
 package com.cst438.service;
 
+import com.cst438.domain.Enrollment;
+import com.cst438.domain.EnrollmentRepository;
+import com.cst438.dto.EnrollmentDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -12,6 +15,8 @@ import org.springframework.stereotype.Service;
 public class GradebookServiceProxy {
 
     Queue gradebookServiceQueue = new Queue("gradebook_service", true);
+    @Autowired
+    private EnrollmentRepository enrollmentRepository;
 
     @Bean
     public Queue createQueue() {
@@ -23,7 +28,22 @@ public class GradebookServiceProxy {
 
     @RabbitListener(queues = "registrar_service")
     public void receiveFromGradebook(String message)  {
-        //TODO implement this message
+        try {
+            System.out.println("Received From Gradebook Service " + message);
+            String[] parts = message.split(",", 2);
+            if (parts[0].equals("updateEnrollment")) {
+                EnrollmentDTO edto = fromJsonString(parts[1], EnrollmentDTO.class);
+                Enrollment e = enrollmentRepository.findById(edto.enrollmentId()).orElse(null);
+                if (e == null) {
+                    System.out.println("receiveFromGradebook Error: Enrollment " + edto.enrollmentId() + " not found");
+                } else {
+                    e.setGrade(edto.grade());
+                    enrollmentRepository.save(e);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Exception in receiveFromGradebook " + e.getMessage());
+        }
     }
 
     private void sendMessage(String s) {
