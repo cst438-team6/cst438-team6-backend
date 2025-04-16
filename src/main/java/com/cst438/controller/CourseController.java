19fdfedfb5a9c57148b83a6170a7aeabe83b321e
Thomas Wood <thwood@csumb.edu>
2025-04-16 14:49:06 -0700

    starting secure branch

--- src/main/java/com/cst438/controller/CourseController.java
+++ src/main/java/com/cst438/controller/CourseController.java
@@ -1,14 +1,15 @@
 package com.cst438.controller;
 
 import com.cst438.domain.*;
-import com.cst438.dto.CourseDTO;
-import com.cst438.dto.SectionDTO;
+import com.cst438.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,8 +37,10 @@ public class CourseController {
 
 
     // ADMIN function to create a new course
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_ADMIN')")
     @PostMapping("/courses")
     public CourseDTO addCourse(@RequestBody CourseDTO course) {
+
         Course c = new Course();
         c.setCredits(course.credits());
         c.setTitle(course.title());
@@ -52,10 +55,12 @@ public class CourseController {
 
     // ADMIN function to update a course
     @PutMapping("/courses")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_ADMIN')")
     public CourseDTO updateCourse(@RequestBody CourseDTO course) {
+
         Course c = courseRepository.findById(course.courseId()).orElse(null);
-        if (c == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "course not found " + course.courseId());
+        if (c==null) {
+            throw  new ResponseStatusException( HttpStatus.NOT_FOUND, "course not found "+course.courseId());
         } else {
             c.setTitle(course.title());
             c.setCredits(course.credits());
@@ -71,16 +76,17 @@ public class CourseController {
     // ADMIN function to delete a course
     // delete will fail if the course has sections
     @DeleteMapping("/courses/{courseid}")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_ADMIN')")
     public void deleteCourse(@PathVariable String courseid) {
         Course c = courseRepository.findById(courseid).orElse(null);
         // if course does not exist, do nothing.
-        if (c != null) {
+        if (c!=null) {
             courseRepository.delete(c);
         }
     }
 
     @GetMapping("/courses")
-    public List<CourseDTO> getAllCourses() {
+    public List<CourseDTO> getAllCourses( ) {
         List<Course> courses = courseRepository.findAllByOrderByCourseIdAsc();
         List<CourseDTO> dto_list = new ArrayList<>();
         for (Course c : courses) {
